0e7f87db223700eab015a2198f62527ee3150cf4
Alex Semenov <aleksey.semenov.fin@gmail.com>
2018-10-28 19:31:36 +0300

    Доработка личной страницы юзера

--- DatingApp.API/ClientApp/src/app/app.module.ts
+++ DatingApp.API/ClientApp/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-import { BsDropdownModule } from 'ngx-bootstrap';
+import { BsDropdownModule, TabsModule } from 'ngx-bootstrap';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
@@ -15,9 +15,20 @@ import { AlertifyService } from './_services/alertify.service';
 import { appRoutes } from './routes';
 import { MessagesComponent } from './messages/messages.component';
 import { ListsComponent } from './lists/lists.component';
-import { MembersComponent } from './members/members.component';
 import { AuthGuard } from './_guards/auth.guard';
+import { UserService } from './_services/user.service';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MemberCardComponent } from './members/member-card/member-card.component';
+import { JwtModule } from '@auth0/angular-jwt';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { MemberDetailResolver } from './_resolves/member-detail.resolver';
+import { MemberListResolver } from './_resolves/member-list.resolver';
+import { NgxGalleryModule } from 'ngx-gallery';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
 
+export function tokenGetter() {
+  return localStorage.getItem('token');
+}
 
 @NgModule({
   declarations: [
@@ -27,20 +38,35 @@ import { AuthGuard } from './_guards/auth.guard';
     HomeComponent,
     MessagesComponent,
     ListsComponent,
-    MembersComponent
+    MemberListComponent,
+    MemberCardComponent,
+    MemberDetailComponent,
+    MemberEditComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
     BsDropdownModule.forRoot(),
-    RouterModule.forRoot(appRoutes)    
+    RouterModule.forRoot(appRoutes),
+    JwtModule.forRoot({
+      config: {
+        tokenGetter: tokenGetter,
+        whitelistedDomains: ['localhost:5000'],
+        blacklistedRoutes: ['localhost:5000/api/auth']
+      }
+    }),
+    TabsModule.forRoot(),
+    NgxGalleryModule
   ],
   providers: [
     AuthService,
     ErrorInterceptorProvide,
     AlertifyService,
-    AuthGuard
+    AuthGuard,
+    UserService,
+    MemberDetailResolver,
+    MemberListResolver
   ],
   bootstrap: [AppComponent]
 })
--- DatingApp.API/ClientApp/src/app/routes.ts
+++ DatingApp.API/ClientApp/src/app/routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { MembersComponent } from './members/members.component';
 import { MessagesComponent } from './messages/messages.component';
 import { ListsComponent } from './lists/lists.component';
 import { AuthGuard } from './_guards/auth.guard';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { MemberDetailResolver } from './_resolves/member-detail.resolver';
+import { MemberListResolver } from './_resolves/member-list.resolver';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
 
 export const appRoutes: Routes = [
   { path: 'home', component: HomeComponent },
@@ -12,7 +16,9 @@ export const appRoutes: Routes = [
     runGuardsAndResolvers: 'always',
     canActivate: [AuthGuard],
     children: [
-      { path: 'members', component: MembersComponent},
+      { path: 'members', component: MemberListComponent, resolve: { users: MemberListResolver } },
+      { path: 'members/:id', component: MemberDetailComponent, resolve: { user: MemberDetailResolver } },
+      { path: 'member/edit', component: MemberEditComponent },
       { path: 'messages', component: MessagesComponent },
       { path: 'lists', component: ListsComponent },
     ]
