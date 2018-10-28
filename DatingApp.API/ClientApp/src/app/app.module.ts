fafaec159f9bec51ed09f026069786b67ef91793
Alex Semenov <aleksey.semenov.fin@gmail.com>
2018-10-28 13:02:30 +0300

    Add routing in Angular app

--- DatingApp.API/ClientApp/src/app/app.module.ts
+++ DatingApp.API/ClientApp/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
@@ -11,6 +12,11 @@ import { RegisterComponent } from './register/register.component';
 import { HomeComponent } from './home/home.component';
 import { ErrorInterceptorProvide } from './_services/error.interceptor';
 import { AlertifyService } from './_services/alertify.service';
+import { appRoutes } from './routes';
+import { MessagesComponent } from './messages/messages.component';
+import { ListsComponent } from './lists/lists.component';
+import { MembersComponent } from './members/members.component';
+import { AuthGuard } from './_guards/auth.guard';
 
 
 @NgModule({
@@ -18,18 +24,23 @@ import { AlertifyService } from './_services/alertify.service';
     AppComponent,
     NavComponent,
     RegisterComponent,
-    HomeComponent
+    HomeComponent,
+    MessagesComponent,
+    ListsComponent,
+    MembersComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
-    BsDropdownModule.forRoot()
+    BsDropdownModule.forRoot(),
+    RouterModule.forRoot(appRoutes)    
   ],
   providers: [
     AuthService,
     ErrorInterceptorProvide,
-    AlertifyService
+    AlertifyService,
+    AuthGuard
   ],
   bootstrap: [AppComponent]
 })
